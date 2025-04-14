90d321544b49715d3d89f314846bd356de7ffda2
Paul Kailer <paulkailer@outlook.de>
2025-04-14 13:09:31 +0200

    remove unused function

--- src/toolkit.rs
+++ src/toolkit.rs
@@ -88,7 +88,7 @@ where
         result
     }
 
-    pub async fn split_vertically(
+    pub async fn partition_vertically(
         &mut self,
     ) -> Result<(DisplayPartition<B, D>, DisplayPartition<B, D>), PartitioningError> {
         let total_area = self.real_display.lock().await.bounding_box();
@@ -106,40 +106,6 @@ where
         let right_partition = self.new_partition(right_area).await?;
 
         Ok((left_partition, right_partition))
-    }
-
-    /// Re-splits an existing partition. Fails if the partition doesn't exist but does not remove
-    /// the current partition's screen access
-    pub async fn split_existing_unchecked(
-        &mut self,
-        existing_area: Rectangle,
-    ) -> Result<(DisplayPartition<B, D>, DisplayPartition<B, D>), PartitioningError> {
-        let mut maybe_i = None;
-        for (i, p) in self.partition_areas.iter().enumerate() {
-            if *p == existing_area {
-                maybe_i = Some(i);
-            }
-        }
-        let Some(existing_i) = maybe_i else {
-            return Err(PartitioningError::ExistingNotFound);
-        };
-
-        self.partition_areas.remove(existing_i);
-
-        let half_width = existing_area.size.width / 2;
-
-        Ok((
-            self.new_partition(Rectangle::new(
-                existing_area.top_left,
-                Size::new(half_width, existing_area.size.height),
-            ))
-            .await?,
-            self.new_partition(Rectangle::new(
-                existing_area.top_left + Point::new(half_width as i32, 0),
-                Size::new(half_width, existing_area.size.height),
-            ))
-            .await?,
-        ))
     }
 
     pub async fn launch_new_app<F>(
